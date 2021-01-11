29d6ca71f356da0da7ca7c0a7c2615f451e63a97
Prasad BKD <prasad.bkd4@gmail.com>
2021-01-11 16:00:06 +0530

    After addding home route and librarian comments routes to the librarian nav

--- src/components/libnav.jsx
+++ src/components/libnav.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
 
  class usernav extends Component {
     click = ()=>{
@@ -16,12 +17,16 @@ import React, { Component } from 'react'
                 </button>
                 <div id="my-nav" className="collapse navbar-collapse">
                 <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                        <a className="nav-link" href="/lib-home" ><HomeRoundedIcon/><span className="sr-only">(current)</span></a>
+                    </li>
                     <li className="nav-item">
                         <a className="nav-link" href="/" onClick={this.click}>{z}Log Out<span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="/commentslib" >Comments📝<span className="sr-only">(current)</span></a>
                     </li>
+                    
                 </ul>
                 </div>
             </nav>
